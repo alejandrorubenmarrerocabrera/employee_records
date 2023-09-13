3a9ee6d7b080da5e34830527f72431bfdf23688e
alejandrorubenmarrerocabrera <alejandrorubenmarrero.cabrera@volenday.com>
2023-09-13 17:12:06 +0100

    Changed to guard clauses to api's endpoints

--- src/pages/api/deleteUser.js
+++ src/pages/api/deleteUser.js
@@ -1,20 +1,19 @@
 import pool from '@/app/database';
 
 export default async function handler(req, res) {
-	if (req.method === 'POST') {
-		try {
-			const { employee_id } = req.body;
-
-			const SQL = 'DELETE FROM employees WHERE employee_id = ?';
-
-			const [result] = await pool.query(SQL, [employee_id]);
-
-			res.status(201).json({ message: 'Employee deleted successfully', result });
-		} catch (error) {
-			console.error('Error creating employee -> ', error);
-			res.status(500).json({ message: 'Employee deletion failed' });
-		}
-	} else {
+	if (req.method !== 'POST') {
 		res.status(405).json({ message: 'Method not allowed' });
+	}
+	try {
+		const { employee_id } = req.body;
+
+		const SQL = 'DELETE FROM employees WHERE employee_id = ?';
+
+		const [result] = await pool.query(SQL, [employee_id]);
+
+		res.status(201).json({ message: 'Employee deleted successfully', result });
+	} catch (error) {
+		console.error('Error creating employee -> ', error);
+		res.status(500).json({ message: 'Employee deletion failed' });
 	}
 }
