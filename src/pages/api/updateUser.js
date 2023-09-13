09117fd8fc55f7c324693f0251b8c6e119ed11ae
alejandrorubenmarrerocabrera <alejandrorubenmarrero.cabrera@volenday.com>
2023-09-13 17:12:06 +0100

    Changed to guard clauses to api's endpoints #5

--- src/pages/api/updateUser.js
+++ src/pages/api/updateUser.js
@@ -1,31 +1,25 @@
 import pool from '@/app/database';
 
 export default async function handler(req, res) {
-	if (req.method === 'POST') {
-		try {
-			const { first_name, last_name, birthday, employee_id } = req.body;
-
-			const SQL = `UPDATE employees
-			SET
-			first_name = COALESCE(?, first_name),
-			last_name = COALESCE(?, last_name),
-			birthday = COALESCE(?, birthday)
-			WHERE employee_id = ?`;
-
-			const [result] = await pool.query(SQL, [
-				first_name || null,
-				last_name || null,
-				birthday || null,
-				employee_id
-			]);
-			pool.query('CALL UpdateEmployeeAge(?)', [employee_id]);
-
-			res.status(201).json({ message: 'Employee updated successfully', result });
-		} catch (error) {
-			console.error('Error creating employee -> ', error);
-			res.status(500).json({ message: 'Employee update failed' });
-		}
-	} else {
+	if (req.method !== 'POST') {
 		res.status(405).json({ message: 'Method not allowed' });
+	}
+	try {
+		const { first_name, last_name, birthday, employee_id } = req.body;
+
+		const SQL = `UPDATE employees
+		SET
+		first_name = COALESCE(?, first_name),
+		last_name = COALESCE(?, last_name),
+		birthday = COALESCE(?, birthday)
+		WHERE employee_id = ?`;
+
+		const [result] = await pool.query(SQL, [first_name || null, last_name || null, birthday || null, employee_id]);
+		pool.query('CALL UpdateEmployeeAge(?)', [employee_id]);
+
+		res.status(201).json({ message: 'Employee updated successfully', result });
+	} catch (error) {
+		console.error('Error creating employee -> ', error);
+		res.status(500).json({ message: 'Employee update failed' });
 	}
 }
