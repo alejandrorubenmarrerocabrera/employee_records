361189fd596ffc2d84b89fb9dc04dea4bf96d840
alejandrorubenmarrerocabrera <alejandrorubenmarrero.cabrera@volenday.com>
2023-09-15 10:43:41 +0100

    Now when a user is created the form is cleaned

--- src/app/components/CreateUser.jsx
+++ src/app/components/CreateUser.jsx
@@ -1,5 +1,6 @@
 'use client';
-import { useRef } from 'react';
+
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useMutation } from '@tanstack/react-query';
 import { Card } from 'antd';
@@ -25,32 +26,73 @@ async function sendUserData(data) {
 }
 
 function UserForm() {
-	const formRef = useRef(null); // Create a ref for the form element
+	const [formData, setFormData] = useState({
+		first_name: '',
+		last_name: '',
+		birthday: ''
+	});
+
 	const { mutate, isLoading, isError, isSuccess } = useMutation(sendUserData);
 
 	const handleSubmit = async event => {
 		event.preventDefault();
 
-		const formData = new FormData(event.target);
 		const data = {
-			first_name: formData.get('first_name'),
-			last_name: formData.get('last_name'),
-			birthday: formData.get('birthday')
+			first_name: formData.first_name,
+			last_name: formData.last_name,
+			birthday: formData.birthday
 		};
-		mutate(data);
 
-		// Reset the form on successful user creation
-		console.log(formRef.current);
+		try {
+			await mutate(data);
+			// Clear the form fields on successful user creation
+			setFormData({
+				first_name: '',
+				last_name: '',
+				birthday: ''
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleChange = event => {
+		const { name, value } = event.target;
+		setFormData(prevData => ({
+			...prevData,
+			[name]: value
+		}));
 	};
 
 	return (
 		<div className="m-2 p-3">
 			<Card>
 				<Meta title="Create User form" description="" />
-				<form onSubmit={handleSubmit} ref={formRef}>
-					<Input type="text" className="m-2 text-black" name="first_name" placeholder="First Name" />
-					<Input type="text" className="m-2 text-black" name="last_name" placeholder="Last Name" />
-					<Input type="date" className="m-2 text-black" name="birthday" placeholder="Birthday" />
+				<form onSubmit={handleSubmit}>
+					<Input
+						type="text"
+						className="m-2 text-black"
+						name="first_name"
+						placeholder="First Name"
+						value={formData.first_name}
+						onChange={handleChange}
+					/>
+					<Input
+						type="text"
+						className="m-2 text-black"
+						name="last_name"
+						placeholder="Last Name"
+						value={formData.last_name}
+						onChange={handleChange}
+					/>
+					<Input
+						type="date"
+						className="m-2 text-black"
+						name="birthday"
+						placeholder="Birthday"
+						value={formData.birthday}
+						onChange={handleChange}
+					/>
 					<button type="submit" className="m-2 p-1 rounded bg-green-500 text-black" disabled={isLoading}>
 						{isLoading ? 'Creating User...' : 'Create User'}
 					</button>
